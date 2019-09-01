0380f07cb49255920eab68f886762d534243fb53
Adrian Setyadi <a.styd@yahoo.com>
2019-09-01 21:38:46 +0700

    Release v0.1.1

--- lib/jsonable/function.rb
+++ lib/jsonable/function.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require 'json' unless defined? ::JSON
+require 'json'
+require 'strscan'
 
 module JSONable
   class Function < Numeric
@@ -43,7 +44,7 @@ module JSONable
     end
 
     def cleanup_function!
-      @function.tr("\n", '').squeeze(' ')
+      function.tr("\n", '').squeeze(' ')
     end
   end
 end
